f6d5072ca8a5cb4a6ec66f8fd9d1ffd5a45bf88b
wsp_1989@126.com <wspgit19891104>
2021-03-04 19:20:12 +0800

    app secret

---
+++ ServiceApp/X.AppSvr/AppSecretSvr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace X.AppSvr
+{
+    class AppSecretSvr
+    {
+        public int ID { get; set; }
+        public string Secret { get; set; }
+        public string SecretPre { get; set; }      
+        public System.DateTime TimeOutTime { get; set; }
+    }
+}
---
+++ ServiceApp/X.AppSvr/DBHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Model.XApp;
+
+namespace X.AppSvr
+{
+    class DBHandler
+    {
+        public AppSecretSvr Find(int AppID)
+        {
+            using(DBXAppContext db=new DBXAppContext()) {
+                return db.AppSecret.Where(T => T.ID == AppID)
+                    .Select(T => new AppSecretSvr() {
+                        ID = T.ID,
+                        Secret = T.Secret,
+                        SecretPre = T.SecretPre,
+                        TimeOutTime = T.TimeOutTime
+                    }).FirstOrDefault();
+            }
+        }
+        object objlock;
+
+        public DBHandler()
+        {
+            objlock = new object();
+        }
+        public void Refresh(int AppID,string Secret,DateTime RefreshTime,DateTime OutTime)
+        {
+            lock (objlock) {
+                using (DBXAppContext context = new DBXAppContext()) {
+                    var app = context.AppSecret.Where(T => T.ID == AppID).FirstOrDefault();
+                    if (app == null) {
+                        app = new AppSecret() {
+                            ID = AppID,
+                            RefreshTime = DateTime.Now,
+                            SecretPre = Secret
+                        };
+                        context.Entry<AppSecret>(app).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                    }
+                    else
+                        context.Entry<AppSecret>(app).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    app.PreTime = app.RefreshTime;
+                    app.SecretPre = app.Secret;
+                    app.RefreshTime = RefreshTime;
+                    app.Secret = Secret;
+                    app.TimeOutTime = OutTime;
+
+                    AppSecretLog log = new AppSecretLog() {
+                        AppID = AppID,
+                        Secret = Secret,
+                        InputTime = DateTime.Now                         
+                    };
+                    context.AppSecretLog.Add(log);
+                    context.SaveChanges();
+                }
+            }            
+        }
+    }
+}
