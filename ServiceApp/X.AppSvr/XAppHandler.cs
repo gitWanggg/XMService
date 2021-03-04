e351445381ebbef9e4c3fef357b12beece368ac7
wsp_1989@126.com <wspgit19891104>
2021-03-04 18:54:34 +0800

    x.app handler

---
+++ ServiceApp/X.AppSvr/XAppHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using X.SDKApp;
+
+namespace X.AppSvr
+{
+    public class XAppHandler : IAppHandler
+    {
+        public Token Find(int AppID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Verify(int AppID, string Sign, string Source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
