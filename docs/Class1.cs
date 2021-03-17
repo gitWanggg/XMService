97bf69cb27e4c69f7c3c05a9813766743b9e5037
gitWanggg <wsp_1989@126.com>
2021-03-17 10:40:35 +0800

    class 1

---
+++ docs/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Stan0.Test
+{
+    public class Class1
+    {
+        public static string FormatUrl(string TmplUrl, Dictionary<string, string> QueryString)
+        {
+            try {
+
+                MatchCollection Matchs = Regex.Matches(TmplUrl, @"\{\s*([\s\S]+?)\}", RegexOptions.IgnoreCase);
+                List<string> pars = new List<string>();
+                foreach (Match item in Matchs) {
+                    string key = item.Groups[1].Value.Trim().Trim('\t').Trim(' ');
+                    string repString = item.Groups[0].Value;
+                    if (QueryString.ContainsKey(key)) {
+                        TmplUrl = TmplUrl.Replace(item.Value, "{" + pars.Count + "}");
+                        pars.Add(System.Web.HttpUtility.UrlEncode(QueryString[key]));
+                    }
+                }
+                return string.Format(TmplUrl, pars.ToArray());
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
