0bae282c51c41c1303b51600573b9ff586e24dea
wsp_1989@126.com <wspgit19891104>
2021-04-20 19:33:49 +0800

    add test config

---
+++ ServiceApp/TestCoreClient/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreClient.Controllers
+{
+    [Route("/{controller}/{action}")]
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return Json("001");
+        }
+    }
+}
