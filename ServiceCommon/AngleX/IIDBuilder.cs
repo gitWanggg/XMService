79539b4dd365fed329d105bb126b5dd4dafa9088
gitWanggg <wsp_1989@126.com>
2022-01-30 21:40:28 +0800

    IIDBuilder

---
+++ ServiceCommon/AngleX/IIDBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngleX
+{
+    public interface IIDBuilder
+    {
+        string NewID<T>() where T : class;
+
+        string NewID<T>(string Format) where T : class;
+
+        string NewID<T>(int Format) where T : class;
+    }
+}
