be0bc3badee3c71c986e737221b4b3e36df7058b
gitWanggg <wsp_1989@126.com>
2022-01-30 12:15:07 +0800

    fist test success

---
+++ XIDServiceLocal/XIDServLocalExtensions/IDServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XIDServiceLocal;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+namespace XIDServLocalExtensions
+{
+    public static class IDServiceExtensions
+    {
+        public static void UseIDServiceLocal(this IServiceCollection services, IConfiguration Configuration)
+        {
+            string dbString = Configuration.GetConnectionString("DBIDService");
+            var optionsBuilder = new DbContextOptionsBuilder<DBIDBuilderContext>();
+            optionsBuilder.UseSqlServer(dbString);
+            DBIDBuilderContext.InitDefaultOp(optionsBuilder.Options);
+            services.AddSingleton<IDCreaterPool>();
+            services.TryAddSingleton<IIDCreaterPool>(serviceProvider => serviceProvider.GetRequiredService<IDCreaterPool>());
+
+        }
+    }
+}
