68b7f2bd16b33ce3eb168914b49535713b2c7bf1
gitWanggg <wsp_1989@126.com>
2022-02-04 14:08:26 +0800

    mysql efcore

--- XIDServiceLocal/IDTestMySql/Startup.cs
+++ XIDServiceLocal/IDTestMySql/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -11,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using XIDServiceLocalMySql;
+using XIDServLocalMySqlExtensions;
 
 namespace IDTestMySql
 {
@@ -27,13 +26,7 @@ namespace IDTestMySql
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string dbString = Configuration.GetConnectionString("DBIDService");
-            var optionsBuilder = new DbContextOptionsBuilder<DBIDBuilderContext>();
-            optionsBuilder.UseMySql(dbString);
-            DBIDBuilderContext.InitDefaultOp(optionsBuilder.Options);
-            services.AddSingleton<IDCreaterPool>();
-            services.TryAddSingleton<IIDCreaterPool>(serviceProvider => serviceProvider.GetRequiredService<IDCreaterPool>());
-
+            services.UseIDServiceLocalMySql(Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
--- XIDServiceLocal/XIDServiceLocalMySql/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace XIDServiceLocalMySql
-{
-    public class Class1
-    {
-    }
-}
